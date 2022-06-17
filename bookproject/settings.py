cd2f34fc6112470a846d4759438739c0ba9d62f7
root <houshanangsuke3@gmail.com>
2022-06-17 21:23:22 +0900

    deploy

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -25,10 +25,10 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-bookproject-tubokotu.herokuapp.com']
-
+ALLOWED_HOSTS = ['*']
+#django-bookproject-tubokotu.herokuapp.com
 
 # Application definition
 
