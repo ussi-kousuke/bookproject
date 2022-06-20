f5228b48bc15e2410484925cf1e65f4f87dd0f25
root <houshanangsuke3@gmail.com>
2022-06-20 13:34:47 +0900

    deploy

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -127,8 +127,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
