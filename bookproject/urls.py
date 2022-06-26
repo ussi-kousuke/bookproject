1dbdae6a13fd5eeb1b3ba3c115c3d8c56bc71cc0
root <houshanangsuke3@gmail.com>
2022-06-26 19:42:34 +0900

    deploy

--- bookproject/urls.py
+++ bookproject/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
